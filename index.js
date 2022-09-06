75ac9f0f68da383c02dbdead03ab6d7b319408cf
Miodini <84649065+Miodini@users.noreply.github.com>
2022-09-06 14:57:41 -0300

    Flappy Bird 2.0 (correction)

--- index.js
+++
@@ -1,3 +0,0 @@
-// Loads the bootstrap tooltip
-const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
-const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
